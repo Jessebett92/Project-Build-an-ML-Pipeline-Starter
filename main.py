404853b118dd75c2f6daf369c286af26ea893f35
Jesse Bettridge <jbettri@wgu.edu>
2026-09-22 03:39:25 -0600

    random_forest_model training implemented

--- main.py
+++ main.py
@@ -93,18 +93,28 @@ def go(config: DictConfig):
         if "train_random_forest" in active_steps:
 
             # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
-            with open(rf_config, "w+") as fp:
+            rf_config_path = os.path.abspath("rf_config.json")
+            with open(rf_config_path, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
-
             ##################
-            # Implement here #
+            _=mlflow.run(
+                f"{config['src_folder']['src_repository']}/train_random_forest",
+                entry_point="main",
+                env_manager="conda",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": str(config["modeling"]["val_size"]),
+                    "random_seed": str(config["modeling"]["random_seed"]),
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config_path,
+                    "max_tfidf_features": str(config["modeling"]["max_tfidf_features"]),
+                    "output_artifact": "random_forest_model",
+                },
+            )
             ##################
-
-            pass
 
         if "test_regression_model" in active_steps:
 
--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -137,6 +137,9 @@ def go(args):
         }
     )
 
+    run.finish()
+
+
 
 def plot_feature_importance(pipe, feat_names):
     # We collect the feature importance for all non-nlp features first
